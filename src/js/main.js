34cbbf0f36b97759c9c232e99ce801ad039333c5
eir73 <roflaneir73@gmail.com>
2020-03-11 11:18:43 +0200

    parallax js

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     // Menu navigate animation
-    document.querySelectorAll(".nav-item>.nav-link").forEach(link => {
-        link.addEventListener("click", function (event) {
+    document.querySelectorAll('.nav-item>.nav-link').forEach(link => {
+        link.addEventListener('click', function (event) {
             event.preventDefault();
-            var id = link.getAttribute("href");
-            document.querySelector(id).scrollIntoView({ block: "start", behavior: "smooth" });
+            var id = link.getAttribute('href');
+            document.querySelector(id).scrollIntoView({ block: 'start', behavior: 'smooth' });
         }, false);
     });
 });
+
+let peaky = document.getElementById('peaky__blinders')
+let man = document.getElementById('man')
+
+
+window.addEventListener('scroll', () => {
+    parallax(pageYOffset)
+})
+
+function parallax(top) {
+    peaky.style.top = 166 + top/2 + 'px'
+    man.style.top = 166 - top/5 + 'px' 
+}
+
+
+
+
 
