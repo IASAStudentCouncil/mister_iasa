30c2b5239d7fafa4c76f78061ee63833d4556726
koval2001 <dianakoval2001@icloud.com>
2020-03-12 22:51:33 +0200

    mobile nav, Danya pochini

--- src/js/main.js
+++ src/js/main.js
@@ -36,4 +36,12 @@ function parallax_mobile(top) {
     })
 }
 
-
+function menu() {
+    let x = document.getElementById("myTopnav");
+    console.log(x)
+    if (x.className === "topnav") {
+        x.className += " responsive"
+    } else {
+        x.className = "topnav"
+    }
+}
