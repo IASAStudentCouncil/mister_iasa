0616bb2a6e05a0b9f7755ac025d8b82d60840da8
eir73 <roflaneir73@gmail.com>
2020-03-19 03:56:31 +0200

    meeting section

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,9 @@ const level = document.getElementById('lvl')
 const mobile_nav = document.getElementById('menu_expand')
 const top_nav = document.getElementById('top_nav')
 const leza = document.querySelector('.leza')
-const header = document.querySelector('header') 
+const header = document.querySelector('header')
+const peaky_mob = document.getElementById('peaky_mob')
+const slider = document.getElementById('slider')
 
 document.addEventListener('DOMContentLoaded', function () {
     // Menu navigate animation
@@ -15,20 +17,45 @@ document.addEventListener('DOMContentLoaded', function () {
             document.querySelector(id).scrollIntoView({ block: 'start', behavior: 'smooth' })
         }, false)
     })
-        
-    header.style.height = screen.height + 'px' // kaef dlya mobilok
+    let swiper = new Swiper('.swiper-container', {
+              slidesPerView: 1,
+              spaceBetween: 30,
+              loop: true,
+              centeredSlides: true,
+              pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+              },
+              navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+              },
 
-    if(document.body.clientWidth > 974) {
-    window.addEventListener('scroll', () => {
-            parallax_desktop(pageYOffset)
         })
+    
+    
+    header.style.height = screen.height + 'px' // kaef dlya mobilok
+    if(document.body.clientWidth > 974) {
+        setInterval(() => {
+            parallax_desktop(pageYOffset)
+        }, 10)
+    } else {
+        //mobile
     }
     mobile_nav.addEventListener('click', menu)
+
  })
 
 function parallax_desktop(offset) {
     peaky.style.top = 100 + offset/2 + 'px'
     man.style.top = 90 - offset/5 + 'px'
+}
+
+function parallax_mobile() {    
+    requestAnimationFrame(() => {
+        level.style.top = pageYOffset * 0.6 + 'px'
+        peaky_mob.style.top = pageYOffset / 2 + 'px'       
+    })
 }
 
 function menu() {
